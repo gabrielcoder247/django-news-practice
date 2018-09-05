536788998f5383afd85f0bcb779dd84387a282e0
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-05 23:16:28 +0300

    function to convertdates to weeks

--- news/views.py
+++ news/views.py
@@ -16,4 +16,13 @@ def news_of_day(request):
         </html>
 
         '''
-    return HttpResponse(html)    
+    return HttpResponse(html) 
+
+def convert_dates(dates):
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+    day = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+
+    # Returning the actual day of the weekday
+    day = days[day_number]
+    return day
