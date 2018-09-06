b7f72877a5f4e9c7f8913340bfa547ac20447f33
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-06 14:06:02 +0300

    create welcome template

--- news/views.py
+++ news/views.py
@@ -25,18 +25,16 @@ def convert_dates(dates):
 
     # Returning the actual day of the weekday
     day = days[day_number]
+
     return day
 
-# def news_of_day(request):
-#     date = dt.date.today()
-#     # Function to convert date object to find exact day
-#     day = convert_dates(date)
-#     html = f'''
-#         <html>
-#             <body>
-#                 <h1>News for {day} {date.day}-{date.month}-{date.year} </h1>
-#             </body>
-#         </html>
-#         '''
-    
-#     return HttpResponse(html)
+def past_days_news(request,past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+
